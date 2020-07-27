5430c54cc7e9c28e99a2bba85b044cac1f390282
Ben <woshiben@hotmail.com>
2020-07-27 10:40:02 +0800

    init

--- WEB-INF/src/resources/core.js
+++ WEB-INF/src/resources/core.js
@@ -1,13 +1,20 @@
 var ioc = {
 		corp : {
 	        type : 'wework.domain.Corp', // 类型
-	        singleton : false, // 是否为单例
+	        singleton : true, // 是否为单例
 	        fields : {
-        	   corpid: 'wwea1a49XXXXXXXXXX',
-        	   applications: [{
-        	      agentId: '100000X',
-        	      secret: 'qAXEXCFZEXXXXX-HwcO-QsBAgtXX4O3-dmFdQacXXXX'
-        	   }]
+	        	   corpid: 'wwea1a49XXXXXXXXXX',
+	        	   applications: {
+	        	      agentId_100000X: {
+	        	         agentId: '100000X',
+	        	         secret: 'qAXEXCFZEXXXXX-HwcO-QsBAgtXX4O3-dmFdQacXXXX',
+	        	      },
+	        	      
+	        	      contacts: {// 通讯录
+	        	         agentId: 'contacts',
+	        	         secret: 'qAXEXCFZEXXXXX-HwcO-QsBAgtXX4O3-dmFdQacYYYY',
+	        	      }
+	        	   }
 	        }
 	    }
 }
